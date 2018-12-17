df1f48d2dfd47bf5b152fa0fa606d26dac3d2b7d
Alexander Lachmann <alexander.lachmann@gmail.com>
2018-12-17 17:36:07 -0500

    modified result object

---
+++ src/jsp/Result.java
@@ -0,0 +1,34 @@
+package jsp;
+
+public class Result implements Comparable<Overlap>{
+	public short[] overlap;
+	public double pval = 0;
+	public int id = 0;
+	public String name = "";
+	public int setsize = 0;
+	public double oddsRatio = 0;
+	public String dbType = "";
+	public int direction = 0;
+	
+	public Result(String _name, short[] _overlap, double _pval, int _setsize, double _odds, int _direction) {
+		pval = _pval;
+		overlap = _overlap;
+		name = _name;
+		setsize = _setsize;
+		oddsRatio = _odds;
+		direction = _direction;
+	}
+	
+	public int compareTo(Overlap over) {
+		if(pval < over.pval) {
+			return -1;
+		}
+		else if(pval > over.pval) {
+			return 1;
+		}
+		else {
+			return 0;
+		}
+	}
+}
+	
