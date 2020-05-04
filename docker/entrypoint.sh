4a7eb86c7d664ff8ff30b6988679f2dc2249c99d
Daniel J. B. Clarke <u8sand@gmail.com>
2020-05-04 13:44:54 -0400

    Ensure PREFIX works with deep prefixes

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -9,11 +9,11 @@ if [ -d "webapps" ]; then
   echo "Cleaning up previous webapps..."
   rm -r webapps
 fi
-mkdir -p webapps
+mkdir -p "webapps$(dirname ${PREFIX})"
 
 echo "Mounting enrichmentapi on ${PREFIX}..."
 
 # link enrichmentapi into webapps directory
-ln -s /enrichmentapi.war webapps${PREFIX}.war
+ln -s /enrichmentapi.war "webapps${PREFIX}.war"
 
 exec "$@"
