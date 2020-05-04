2cb4819593a7a32466097f0b43732172be8dda9d
Daniel J. B. Clarke <u8sand@gmail.com>
2020-05-04 14:13:31 -0400

    Attempt #2 to get PREFIX working with deep prefixes

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -9,11 +9,15 @@ if [ -d "webapps" ]; then
   echo "Cleaning up previous webapps..."
   rm -r webapps
 fi
-mkdir -p "webapps$(dirname ${PREFIX})"
+
+# sub-paths in tomcat defined with this#sub#path.war
+WEBAPPS_PATH="webapps/$(echo ${PREFIX} | cut -b2- | sed 's/\//#/g').war"
+
+mkdir -p $(dirname ${WEBAPPS_PATH})
 
 echo "Mounting enrichmentapi on ${PREFIX}..."
 
 # link enrichmentapi into webapps directory
-ln -s /enrichmentapi.war "webapps${PREFIX}.war"
+ln -s /enrichmentapi.war "${WEBAPPS_PATH}"
 
 exec "$@"
