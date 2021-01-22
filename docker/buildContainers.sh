9aa9764f9fea47f2e16db28df9188728a78c4e53
Alexander Lachmann <32603869+lachmann12@users.noreply.github.com>
2021-01-22 11:13:29 -0500

    update gradle version

--- docker/buildContainers.sh
+++ docker/buildContainers.sh
@@ -5,11 +5,7 @@ docker rm $(docker ps -q -f status=exited)
 docker kill enrichmentapi
 docker rm enrichmentapi
 
-#docker build -f DockerStar -t maayanlab/aligner-amazon .
 docker build -f DockerfileAPI -t maayanlab/enrichmentapi .
 
-#docker push maayanlab/aligner-amazon
 docker push maayanlab/enrichmentapi
-
-#docker run -d --name="jobscheduler" -p 8989:80 maayanlab/awsjobscheduler
 
