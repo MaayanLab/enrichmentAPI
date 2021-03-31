5ef195ee4004db2478aaf88763f568da442675c5
Alexander Lachmann <32603869+lachmann12@users.noreply.github.com>
2021-03-31 11:21:57 -0400

    Update buildContainers.sh

--- docker/buildContainers.sh
+++ docker/buildContainers.sh
@@ -5,7 +5,7 @@ docker rm $(docker ps -q -f status=exited)
 docker kill enrichmentapi
 docker rm enrichmentapi
 
-docker build -f DockerfileAPI -t maayanlab/enrichmentapi .
+docker build -f DockerfileAPI -t maayanlab/enrichmentapi:1.5.4 .
 
-docker push maayanlab/enrichmentapi
+docker push maayanlab/enrichmentapi:1.5.4
 
